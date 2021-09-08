6c6fb845f7159205919b2a45855914bbbe556a6a
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-09-08 09:18:21 +0700

    24: update pom file: adding 2 spring dependencies

---
+++ src/main/java/com/mycompany/spring03_04_setterinjection/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.spring03_04_setterinjection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
