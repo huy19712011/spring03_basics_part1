ba8038bc1dc0c29be420ab983fa767b02ba95a57
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-09-10 21:29:51 +0700

    79: Autowiring using Constructor

--- src/main/java/S09_autowiring/Employee.java
+++ src/main/java/S09_autowiring/Employee.java
@@ -15,6 +15,11 @@ public class Employee {
 
     private Address address;
 
+    public Employee(Address address) {
+        this.address = address;
+    }
+
+
     @Override
     public String toString() {
         return "Employee{" + "address=" + address + '}';
