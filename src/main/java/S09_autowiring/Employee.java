01df32064fd8167fbd2c04b50debf715b432b3f1
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-09-10 21:13:51 +0700

    76: Autowiring by Type

---
+++ src/main/java/S09_autowiring/Employee.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S09_autowiring;
+
+
+
+/**
+ *
+ * @author huynq
+ */
+public class Employee {
+
+    private Address address;
+
+    @Override
+    public String toString() {
+        return "Employee{" + "address=" + address + '}';
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
+}
---
+++ src/main/java/S09_autowiring/TestRunner.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S09_autowiring;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ *
+ * @author huynq
+ */
+public class TestRunner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+//        Path path = Paths.get("config.xml");
+//        System.out.println(path);
+
+//        // Create a file object
+//            File f = new File("config.xml");
+//
+//            // Get the absolute path of file f
+//            System.out.println(f.getAbsolutePath());
+
+//    System.out.println(new File(".").getAbsolutePath());
+
+
+        // put .xml file into src/main/resources folder
+
+        ApplicationContext ctx
+                = new ClassPathXmlApplicationContext("autowiring_config.xml");
+
+        Employee bean
+                = ctx.getBean("employee", Employee.class);
+        System.out.println(bean);
+
+
+
+
+
+
+
+//        // works on both netbeans and eclipse
+//        String path = "D:\\Projects\\Java\\javaSpring\\spring03_04_SetterInjection\\src\\main\\java\\S05_life_cycle_methods\\annotations\\annotations_config.xml";
+//
+//        // change to AbstractApplicationContext
+//        AbstractApplicationContext context = new FileSystemXmlApplicationContext(path);
+//        Patien bean
+//                = context.getBean("patien", Patien.class);
+//
+//        System.out.println(bean);
+//
+//        // set to enable destroy method
+//        context.registerShutdownHook();
+
+
+    }
+
+}
