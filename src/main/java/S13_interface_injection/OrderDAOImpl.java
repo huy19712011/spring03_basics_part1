229a5132806a61d6b08c29e3ac2b82cfe96fd471
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-09-11 11:13:24 +0700

    100: Injecting interfaces introduction - creating POJOs

---
+++ src/main/java/S13_interface_injection/OrderDAOImpl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S13_interface_injection;
+
+/**
+ *
+ * @author huynq
+ */
+public class OrderDAOImpl implements OrderDAO {
+
+    @Override
+    public void createOrder() {
+
+        System.out.println("Inside Order DAO createOrder()");
+    }
+
+}
